050c2c72db9e8f456eaabe93f268726e21be4f43
DingZhouPeng <dzp@njust.edu.cn>
2026-06-23 12:15:35 +0800

    Updates

--- assets/IslandScene3D-CmVGibnx.js
+++ assets/IslandScene3D-CmVGibnx.js
@@ -1,4 +1,4 @@
-import{r as Ue,g as ix,j as ie,a as rx,b as sx}from"./index-Bdl4pJFT.js";/**
+import{r as Ue,g as ix,j as ie,a as rx,b as sx}from"./index-DL0TNiYJ.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
