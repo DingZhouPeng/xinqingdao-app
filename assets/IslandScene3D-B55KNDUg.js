398b1c46e3624992797976be7324d71d2fb0c586
DingZhouPeng <dzp@njust.edu.cn>
2026-06-23 12:02:33 +0800

    Updates

--- assets/IslandScene3D-B55KNDUg.js
+++ assets/IslandScene3D-B55KNDUg.js
@@ -1,4 +1,4 @@
-import{r as Ue,g as ix,j as ie,a as rx,b as sx}from"./index-BKu5U-hP.js";/**
+import{r as Ue,g as ix,j as ie,a as rx,b as sx}from"./index-Bdl4pJFT.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
